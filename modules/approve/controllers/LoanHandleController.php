5a5cc57d96f96c1007de97822ed8dd84ea20d938
wujiepeng <517987404@qq.com>
2018-12-26 23:16:35 +0800

    1、审批后台列表页字段展示2、审批后台列表页中添加查看资料详情

--- modules/approve/controllers/LoanHandleController.php
+++ modules/approve/controllers/LoanHandleController.php
@@ -30,9 +30,15 @@ class LoanHandleController extends CommonController
         $approve_user_id = Yii::$app->approvr_user->identity->id;
         $pageSize = 20;
         $query = WorkflowLog::find()->alias('wfl')
-                ->select(['wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id', 'wfl.group_id', 'wfl.node_id', 'wfl.is_read', 'b.*', 'l.*'])
+                ->select([
+                    'wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id', 'wfl.group_id', 'wfl.node_id', 'wfl.is_read',
+                    'b.base_id', 'b.enterprise_name', 'b.contact_person_man', 'b.contact_person_phone', 'b.base_create_time',
+                    'l.loan_id', 'l.apply_amount', 'l.period_month',
+                    'tl.name town_name',
+                ])
                 ->leftJoin('{{%enterprise_base}} b', 'b.base_id=wfl.app_id')
                 ->leftJoin('{{%enterprise_loan}} l', 'l.base_id=wfl.app_id')
+                ->leftJoin('{{%town_list}} tl', 'tl.id=b.town_id')
                 ->where(['AND', ['wfl.user_id' => $approve_user_id], ['OR', ['wfl.result' => null], ['wfl.result' => '']], ['wfl.group_id' => $this->loan_group_id]]);
         $countQuery = clone $query;
         $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => $pageSize]);
@@ -56,8 +62,15 @@ class LoanHandleController extends CommonController
         $app_ids = WorkflowLog::find()->select('app_id')->where(['user_id' => Yii::$app->approvr_user->identity->id])->asArray()->column();
         $pageSize = 20;
         $query = WorkflowLog::find()->alias('wfl')
-                ->select(['wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id', 'b.*'])
+                ->select([
+                    'wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id',
+                    'b.base_id', 'b.enterprise_name', 'b.contact_person_man', 'b.contact_person_phone', 'b.base_create_time',
+                    'l.loan_id', 'l.apply_amount', 'l.period_month',
+                    'tl.name town_name',
+                ])
                 ->leftJoin('{{%enterprise_base}} b', 'b.base_id=wfl.app_id')
+                ->leftJoin('{{%enterprise_loan}} l', 'l.base_id=wfl.app_id')
+                ->leftJoin('{{%town_list}} tl', 'tl.id=b.town_id')
                 ->where(['AND', ['IN', 'wfl.app_id', $app_ids], ['wfl.result' => 'end'], ['wfl.group_id' => $this->loan_group_id]]);
         $countQuery = clone $query;
         $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => $pageSize]);
@@ -73,8 +86,15 @@ class LoanHandleController extends CommonController
         $app_ids = WorkflowLog::find()->select('app_id')->where(['user_id' => Yii::$app->approvr_user->identity->id])->asArray()->column();
         $pageSize = 20;
         $query = WorkflowLog::find()->alias('wfl')
-                ->select(['wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id', 'b.*'])
+                ->select([
+                    'wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id',
+                    'b.base_id', 'b.enterprise_name', 'b.contact_person_man', 'b.contact_person_phone', 'b.base_create_time',
+                    'l.loan_id', 'l.apply_amount', 'l.period_month',
+                    'tl.name town_name',
+                ])
                 ->leftJoin('{{%enterprise_base}} b', 'b.base_id=wfl.app_id')
+                ->leftJoin('{{%enterprise_loan}} l', 'l.base_id=wfl.app_id')
+                ->leftJoin('{{%town_list}} tl', 'tl.id=b.town_id')
                 ->where(['AND', ['IN', 'wfl.app_id', $app_ids], ['wfl.result' => 'back'], ['wfl.group_id' => $this->loan_group_id]]);
         $countQuery = clone $query;
         $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => $pageSize]);
@@ -92,8 +112,15 @@ class LoanHandleController extends CommonController
         $app_ids = WorkflowLog::find()->select('app_id')->where(['user_id' => Yii::$app->approvr_user->identity->id])->asArray()->column();
         $pageSize = 20;
         $query = WorkflowLog::find()->alias('wfl')
-                ->select(['wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id', 'b.*'])
+                ->select([
+                    'wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id',
+                    'b.base_id', 'b.enterprise_name', 'b.contact_person_man', 'b.contact_person_phone', 'b.base_create_time',
+                    'l.loan_id', 'l.apply_amount', 'l.period_month',
+                    'tl.name town_name',
+                ])
                 ->leftJoin('{{%enterprise_base}} b', 'b.base_id=wfl.app_id')
+                ->leftJoin('{{%enterprise_loan}} l', 'l.base_id=wfl.app_id')
+                ->leftJoin('{{%town_list}} tl', 'tl.id=b.town_id')
                 ->where(['AND', ['IN', 'wfl.app_id', $app_ids], ['wfl.result' => 'defer'], ['wfl.group_id' => $this->loan_group_id]]);
         $countQuery = clone $query;
         $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => $pageSize]);
@@ -123,9 +150,15 @@ class LoanHandleController extends CommonController
         $app_ids = WorkflowLog::find()->select('app_id')->where(['user_id' => Yii::$app->approvr_user->identity->id])->asArray()->column();
         $pageSize = 20;
         $query = WorkflowLog::find()->alias('wfl')
-                ->select(['wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id', 'b.*', 'l.*'])
+                ->select([
+                    'wfl.id workflow_log_id', 'wfl.app_id', 'wfl.group_id', 'wfl.user_id approve_user_id',
+                    'b.base_id', 'b.enterprise_name', 'b.contact_person_man', 'b.contact_person_phone', 'b.base_create_time',
+                    'l.loan_id', 'l.apply_amount', 'l.period_month',
+                    'tl.name town_name',
+                ])
                 ->leftJoin('{{%enterprise_base}} b', 'b.base_id=wfl.app_id')
                 ->leftJoin('{{%enterprise_loan}} l', 'l.base_id=wfl.app_id')
+                ->leftJoin('{{%town_list}} tl', 'tl.id=b.town_id')
                 ->where(['AND', ['IN', 'wfl.app_id', $app_ids], ['OR', ['wfl.result' => 'finish'], ['wfl.result' => 'grant']], ['wfl.group_id' => $this->loan_group_id]]);
         $countQuery = clone $query;
         $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => $pageSize]);
