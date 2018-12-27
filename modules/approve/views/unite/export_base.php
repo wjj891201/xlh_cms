5e6a3acf8bfd3cd023316977426f8ad80597a50c
chenxin <chenxin@xinlonghang.cn>
2018-12-27 11:29:50 +0800

    pdf导出

---
+++ modules/approve/views/unite/export_base.php
@@ -0,0 +1,17 @@
+<style>
+    table {font-family:微软雅黑; width:100% border-collapse:collapse; font-size:12px; border-color:#000000;}
+    td {padding: 10px; line-height:35px;}
+    p {text-align: left; margin-left:50px;}
+    strong {text-align:center;}
+</style>
+<p align="center" style="font-size:20px;margin:0px 0px 10px 0px;"><strong>企业入库申请表</strong></p>
+<table border="1" cellspacing="0">
+    <tr>
+        <td width="150"><strong> 企业名称 </strong></td>
+        <td width="500"><p> qqqqq </p></td>
+    </tr>
+    <tr>
+        <td width="150"><strong> 所属区域 </strong></td>
+        <td width="500"><p> qqqqq </p></td>
+    </tr>
+</table>
